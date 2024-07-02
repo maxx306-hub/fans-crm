fec657419b17c65b7b43aa4312e703943e1baac3
maxx306.hub <muzonchikplay@gmail.com>
2024-07-01 22:01:18 -0400

    feat(db): configuration for database

--- packages/fans-crm-api/src/app.module.ts
+++ packages/fans-crm-api/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.production'],
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
--- packages/fans-crm-api/src/main.ts
+++ packages/fans-crm-api/src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start() {
+  const PORT = process.env.PORT;
+  const app = await NestFactory.create(AppModule, {})
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
-bootstrap();
+
+start()
